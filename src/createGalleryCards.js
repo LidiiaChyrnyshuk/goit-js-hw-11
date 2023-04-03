c6ff4fd2513c76af3d64a4be00a0a5a81afd8ea9
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-04-03 16:00:22 +0300

    rerecorded markup

--- src/createGalleryCards.js
+++ src/createGalleryCards.js
@@ -6,30 +6,35 @@ export function createGalleryCards({
   views,
   comments,
   downloads }) {
-  return `<div class="photo-card">
-                <a class="gallery-link" href="${largeImageURL}" >
-                  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="gallery-img img" />
-                </a>
-                
-                <div class="info">
-                  <p class="info-item">
-                    <b>Likes</b>
-                    ${likes}
-                  </p>
-                  <p class="info-item">
-                    <b>Views</b>
-                    ${views}
-                  </p>
-                  <p class="info-item">
-                    <b>Comments</b>
-                    ${comments}
-                  </p>
-                  <p class="info-item">
-                    <b>Downloads</b>
-                    ${downloads}
-                  </p>
-                </div>
-              </div>`;
+  return `<a class="gallery-link" href="${largeImageURL}">
+            <div class="photo-card">
+              <img
+                src="${webformatURL}"
+                alt="${tags}"
+                loading="lazy"
+                class="gallery-img img"
+              />
+
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes</b>
+                  ${likes}
+                </p>
+                <p class="info-item">
+                  <b>Views</b>
+                  ${views}
+                </p>
+                <p class="info-item">
+                  <b>Comments</b>
+                  ${comments}
+                </p>
+                <p class="info-item">
+                  <b>Downloads</b>
+                  ${downloads}
+                </p>
+              </div>
+            </div>
+          </a>`;
   }
 
 
